709bb41068a079f631e9589325193d3d413d53d6
Sid K <sidkhillon24@gmail.com>
2019-09-13 16:55:25 -0700

    Mecanum update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -8,18 +8,24 @@ public class Mecanum {
     private Vector velocity = new Vector();
     private float rotation = 0.0f;
 
+    Motor backLeft;
+    Motor backRight;
+    Motor frontLeft;
+    Motor frontRight;
+
     public Mecanum(Component backLeft, Component backRight, Component frontLeft, Component frontRight){
-        motors[0] = (Motor) backLeft;
-        motors[1] = (Motor) backRight;
-        motors[2] = (Motor) frontLeft;
-        motors[3] = (Motor) frontRight;
+        this.backLeft = (Motor) backLeft;
+        this.backRight = (Motor) backRight;
+        this.frontLeft = (Motor) frontLeft;
+        this.frontRight = (Motor) frontRight;
     }
 
-    public void moveLinear(float xMove, float yMove) {
-        
+    public void move(float xMove, float yMove, float rotate) {
+        backLeft.setSpeed(velocity.getY() + rotate);
+        backRight.setSpeed(velocity.getX() - rotate);
+        frontLeft.setSpeed(velocity.getX() + rotate);
+        frontRight.setSpeed(velocity.getY() - rotate);
+
     }
 
-    public void rotate(float rotate) {
-
-    }
 }
