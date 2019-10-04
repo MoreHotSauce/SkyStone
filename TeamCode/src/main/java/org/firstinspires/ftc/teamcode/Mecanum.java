24f6b23dac180790b6ad756c4c8a8c0b8e5b8629
Sid K <sidkhillon24@gmail.com>
2019-10-04 15:59:43 -0700

    turbo pt3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -15,10 +15,6 @@ public class Mecanum {
 
     private boolean turbo;
 
-    public boolean isTurbo() {
-        return turbo;
-    }
-
     public void setTurbo(boolean turbo) {
         this.turbo = turbo;
     }
