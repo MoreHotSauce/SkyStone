118ddbc9e8970a9afbd6df48e7e7884cbb781dd4
Sid K <sidkhillon24@gmail.com>
2020-01-29 21:47:30 -0800

    skystone detection OpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneDetectionHelper.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.disnodeteam.dogecv.detectors.skystone.SkystoneDetector;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import java.util.Locale;
+
+public class SkystoneDetectionHelper {
+    private OpenCvCamera phoneCam;
+    private SkystoneDetector skyStoneDetector;
+
+    public SkystoneDetectionHelper(HardwareMap map){
+        int cameraMonitorViewId = map.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", map.appContext.getPackageName());
+        phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam.openCameraDevice();
+        skyStoneDetector = new SkystoneDetector();
+        phoneCam.setPipeline(skyStoneDetector);
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    }
+
+    public float getStoneX() {
+        return (float) skyStoneDetector.getScreenPosition().x;
+    }
+
+    public float getStoneY() {
+        return (float) skyStoneDetector.getScreenPosition().y;
+    }
+
+    public int skystonePos() {
+        if (this.getStoneX() < 100) { //BRIDGE
+            return 1;
+        } else if (this.getStoneX() > 100 && this.getStoneX() < 200){ //MIDDLE
+            return 2;
+        } else {
+            return 3; //WALL
+        }
+    }
+
+}
