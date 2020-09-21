b2e987b30f58f2742845d45249f4148260e99f73
Niket Patel <niket_p@outlook.com>
2020-09-21 12:12:31 -0700

    go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -43,12 +43,6 @@ public class Main extends OpMode{
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
         camera.openCameraDevice();
-
-        pipeline = new SkystoneDetector(25, 25, 50, 50, null);
-
-        camera.setPipeline(pipeline);
-        camera.startStreaming(640, 480, OpenCvCameraRotation.UPRIGHT);
-
     }
 
     public void start(){
@@ -58,24 +52,6 @@ public class Main extends OpMode{
 
     @Override
     public void loop() {
-        robot.turbo(gamepad1.right_bumper);
-
-        robot.intakeControl(gamepad2.a || gamepad1.a);
-
-        robot.huggerControl(gamepad2.x, gamepad2.b);
-
-        robot.actuatorControl(gamepad2.dpad_up, gamepad2.dpad_down);
-
-        robot.switchHuggers(gamepad2.dpad_left, gamepad2.dpad_right);
-
-        robot.moveLift(gamepad2.left_trigger, gamepad2.right_trigger);
-
-        robot.foundationHookControl(gamepad2.y);
-
-        if(gamepad2.left_bumper){
-            robot.moveLift(1,0);
-            robot.actuatorControl(true,false);
-        }
 
         if(gamepad1.dpad_down){
             robot.drive(0, 0.4f, 0f);
@@ -93,11 +69,8 @@ public class Main extends OpMode{
             //robot.changeTarget(0,0);
         }
 
-        telemetry.addData("rot", robot.currentR);
-        telemetry.addData("limit", robot.limit.digitalTouch.isPressed());
         telemetry.addData("x", robot.getOdoX());
         telemetry.addData("y", robot.getOdoY());
-        telemetry.addData("Detection", pipeline.getSkystonePosition());
         //telemetry.addData("y", robot.limit.digitalTouch.isPressed());
     }
 }
